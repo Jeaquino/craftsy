5c263e2d8fda50896737fcbd4992c02881c118ad
Julian E. Aquino <julian.eduardo.aquino@gmail.com>
2023-10-31 19:05:52 -0300

    add registro

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const path = require ('path')
+const path = require ('path');
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3030;
+
 app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/',(req,res)=> res.sendFile(path.join(__dirname, 'views','home.html')))
+app.get('/registro',(req,res)=> res.sendFile(path.join(__dirname, 'views','registro.html')))
+app.post('/registro',(req,res)=> {
+    console.log(req.body);
+    res.redirect("/")
+})
 app.listen(PORT,() => console.log(`server running http://localhost:${PORT}`));
